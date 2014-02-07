bf78bca62846bc0d0da2487601c6e72da15954ce
mconcas <mett1990@gmail.com>
2014-02-07 12:08:41 +0100

    ProtoClass SessionLog and minor bugs

---
+++ cpp/scratches/Hit.cxx
@@ -0,0 +1,146 @@
+#if !defined (__CINT__) || defined (__MAKECINT__)
+#include "Punto.h"
+#include "Vertice.h"
+#include "Hit.h"
+#include "Direzione.h"
+#include <TMath.h>
+#include <TMaterial.h>
+#include <TRandom3.h>
+#include <TSystem.h>
+#endif
+
+ClassImp (Hit)
+
+Hit::Hit(): Punto(),
+   layernum(0),
+   idnumber(0),
+   realhit(kFALSE) { }
+
+Hit::Hit(Double_t fX, Double_t fY, Double_t fZ, Int_t fLayer, 
+         Int_t fIdnumber ): 
+   Punto(fX, fY, fZ),
+   layernum(fLayer),
+   idnumber(fIdnumber),
+   realhit(kFALSE) { }
+
+Hit::~Hit() {
+   if (gDebug) Printf( "*** hit object destroyed ***" );
+}
+
+void Hit::NowRealHit() { realhit=kTRUE; }
+void Hit::SetHitID(Int_t fId) { idnumber=fId; }
+void Hit::SetHitLayno(Int_t fLay) { layernum=fLay; }
+
+Double_t Hit::ComputeT(Double_t fTheta, Double_t fPhi,
+   Double_t fXO, Double_t fYO, Double_t fRadius) {
+      const Double_t sintheta=TMath::Sin(fTheta);
+      const Double_t sinphi=TMath::Sin(fPhi);
+      const Double_t cosphi=TMath::Cos(fPhi);
+      const Double_t t=(-(fXO*cosphi+fYO*sinphi)
+            +TMath::Sqrt((fXO*cosphi+fYO*sinphi)*(fXO*cosphi
+            +fYO*sinphi)-fXO*fXO-fYO*fYO+fRadius*fRadius))/sintheta;
+
+      return t;
+}
+
+Hit *Hit::HitOnCylFromVertex(Vertice &fOrigin, Direzione &fDirect,
+   Double_t fRadius, Int_t fIde, Int_t fLayno) {
+
+      // Extract theta and phi from "Direzione".
+      const Double_t fThe=fDirect.GetDirectTheta();
+      const Double_t fPhi=fDirect.GetDirectPhi();
+      
+      ////////////////////////////////////////////////////////////////
+      // Manage the ϑ=0 exception
+      if(fThe==0 || fThe==TMath::Pi()) { 
+         Hit *OnCyl=new Hit();
+         if (gDebug) Printf("ϑ=0 exception -> no scattering.");
+         return OnCyl;
+      }   
+      else {
+
+      ////////////////////////////////////////////////////////////////
+      // Get origin coordinates: xO and yO from "Vertice"
+      const Double_t xO=fOrigin.GetPuntoX();
+      const Double_t yO=fOrigin.GetPuntoY();
+      const Double_t zO=fOrigin.GetPuntoZ();
+      
+      ////////////////////////////////////////////////////////////////
+      // Compute the "t" value.
+      const Double_t t=ComputeT(fThe,fPhi,xO,yO,fRadius);
+
+      // Build item in return.
+      Hit *OnCyl=new Hit( xO+t*fDirect.GetDirCos1(), 
+                          yO+t*fDirect.GetDirCos2(), 
+                          zO+t*fDirect.GetDirCos3(),
+                          fLayno, fIde );
+
+      return OnCyl;
+   }  
+}
+
+Hit *Hit::GetHitOnCyl(Direzione &fDirect, Double_t fRadius, 
+                      TMaterial &fMaterial, Double_t fWidth, 
+                      Int_t fIde, Bool_t multscat, Int_t fLayno, 
+                      Double_t fP, Int_t fZ, Double_t fBeta) {
+   if (multscat) {
+
+      /////////////////////////////////////////////////////////////
+      // θo represents the rms of a gaussian distribution centred 
+      // in 0.
+      // Formula used for the MScattering θo evaluation:
+      // Dimensionalities:
+      // [x]=cm ; [X_0]=cm; [c]=3x10**8; [b]=#; [fP]=MeV/c
+      // θo = (13.6 MeV/β*c*p)*Z*(√(x/X_0))[1+0.038*Ln(x/X_0)]
+      /////////////////////////////////////////////////////////////
+      // Get the Radiation Length: X_0
+      const Double_t X_0 = fMaterial.GetRadLength();
+      
+      /////////////////////////////////////////////////////////////  
+      // WARNING!
+      // Since fP should be passed with a dimensionality of [fP] 
+      // [fP]= MeV/c it's obvious that "c" wouldn't appear in this 
+      // computation.
+      const Double_t fThetaZero=(13.6/(fBeta*fP))*fZ*\
+      TMath::Sqrt(fWidth/X_0)*(1+0.038*TMath::Log(fWidth/X_0));
+      
+      /////////////////////////////////////////////////////////////
+      // Generate θ form Gaussian distribution with mean 0 and 
+      // rms θo. Phi is uniformely distributed between 0<=φ<2Pi
+      const Double_t thetalocal=gRandom->Gaus(0.,fThetaZero); 
+      const Double_t philocal=gRandom->Rndm()*2*TMath::Pi();
+
+      /////////////////////////////////////////////////////////////
+      // Rotate direction in order to refer it to the Laboratory 
+      // Reference System.
+      fDirect.Direzione::Rotate(thetalocal, philocal);
+
+   }  
+
+   // Extract theta and phi from "Direzione".
+   const Double_t fThe=fDirect.GetDirectTheta();
+   const Double_t fPhi=fDirect.GetDirectPhi();
+
+   ////////////////////////////////////////////////////////////////
+   // Manage the ϑ=0 exception
+   if(fThe==0 || TMath::Abs(fThe)==TMath::Pi()) { 
+      Hit *OnCyl=new Hit();
+      if (gDebug) Printf("ϑ=0 exception -> no scattering.");
+
+      return OnCyl;
+   }  
+   else {
+
+      /////////////////////////////////////////////////////////////
+      // Compute "t" value.
+      const Double_t t=ComputeT(fThe,fPhi,X,Y,fRadius);
+         
+      // Returned item.
+      Hit *OnCyl=new Hit( X+t*fDirect.GetDirCos1(), 
+                          Y+t*fDirect.GetDirCos2(),
+                          Z+t*fDirect.GetDirCos3(),
+                          fLayno, fIde );
+            
+         return OnCyl;
+   }
+}    
---
+++ cpp/scratches/Punto.cxx
@@ -0,0 +1,217 @@
+#if !defined (__CINT__) || defined (__MAKECINT__)
+#include <TMath.h>
+#include "Punto.h"
+#include <TObject.h>
+#include <TString.h>
+#include <TSystem.h>
+#endif
+
+ClassImp(Punto)
+
+//___________{De,Con}structors_______________
+Punto::Punto() : TObject(),
+   X(0.),
+   Y(0.),    
+   Z(0.),
+   Theta(0.),
+   Phi(0.),
+   CRadius(0.),
+   SRadius(0.) { }
+
+Punto::Punto(Double_t fX, Double_t fY, Double_t fZ): 
+   TObject(),
+   X(fX),
+   Y(fY),
+   Z(fZ) { 
+      CartesiantoSpherical();   // Set Spherical coords.
+      CartesiantoCylindrical(); // Set Cylindrical coords.
+   }
+
+//_________Setters___________________________
+void Punto::SetPuntoCartesians(const Double_t fX, const Double_t fY, 
+   const Double_t fZ) {
+      X=fX;
+      Y=fY;
+      Z=fZ;
+      CartesiantoSpherical();
+      CartesiantoCylindrical();
+}
+
+void Punto::SetPuntoCylindricals(const Double_t fZ, 
+   const Double_t fCRadius, const Double_t fPhi) {
+      Z=fZ;
+      Phi=fPhi;
+      CRadius=fCRadius;
+      CylindricaltoCartesian();
+      CartesiantoSpherical();
+}
+
+void Punto::SetPuntoSphericals(const Double_t fTheta, 
+   const Double_t fPhi, const Double_t fSRadius) {
+      Theta=fTheta;
+      Phi=fPhi;
+      SRadius=fSRadius;
+      SphericaltoCartesian();
+      CartesiantoCylindrical();
+}
+
+void Punto::SetPuntoX(const Double_t fX) {
+   X=fX;
+   CartesiantoSpherical();
+   CartesiantoCylindrical();
+}
+
+void Punto::SetPuntoY(const Double_t fY) {
+   Y=fY;
+   CartesiantoSpherical();
+   CartesiantoCylindrical();
+}
+
+void Punto::SetPuntoZ(const Double_t fZ) {
+   Z=fZ;
+   CartesiantoSpherical();
+   // Please note that is not necessary to update cylindrical 
+   // coordinates because Z is the same coord.    
+}
+
+void Punto::SetPuntoTheta(const Double_t fTheta) {
+
+   ///////////////////////////////////////////////////////////////////
+   //Theta is a spherical parameter. We have to invert the spherical 
+   //relations due to be effective the cartesian coords update. 
+   //For some reason one might have to modify just one spherical 
+   //coordinate. It is fine, if just working with sph coordinates. 
+   //To keep consistency it is necessary to update the cartesian ones.
+   //Then it's possible apply a CartesiantoCylindrical conversion for 
+   //an update to cylindrical coords.
+   ///////////////////////////////////////////////////////////////////
+   Theta=fTheta;
+   SphericaltoCartesian();
+   CartesiantoCylindrical();
+}
+
+void Punto::SetPuntoPhi(const Double_t fPhi) {
+
+   ///////////////////////////////////////////////////////////////////
+   //fPhi is a spherical and a cylindrical parameter. 
+   //We have to invert the cylindrical relations due to make effective 
+   //the cartesian coords update. 
+   //For some reason one might have to modify just a cylindrical 
+   //coordinate. It is fine, if just working with cyl coordinates. 
+   //To keep consistency it is necessary to update the cartesian ones.
+   //Then it's possible apply a CartesiantoCylindrical conversion for 
+   //an update to cylindrical coords.
+   ///////////////////////////////////////////////////////////////////
+   Phi=fPhi;
+   CylindricaltoCartesian();
+   CartesiantoSpherical();
+}
+
+void Punto::SetPuntoCRadius(const Double_t fCRadius) {
+   CRadius=fCRadius;
+   if (gDebug && X==0 && Y==0) 
+      Printf("Warning: Cartesian variables X=Y=0! \
+               \nThis assignment is probably \
+               \ngoing to make a mess with datamembers!\\n\
+               \nPlease, check your intentions and the source code!");
+   CylindricaltoCartesian();
+   CartesiantoSpherical();
+   if (gDebug) Printf("Debug: \n X=%f\n Y=%f\n Z=%f",X,Y,Z); 
+}
+
+void Punto::SetPuntoSRadius(const Double_t fSRadius) {
+   SRadius=fSRadius;
+   if (gDebug && X==0 && Y==0 && Z==0) 
+      Printf("Warning: Cartesian variables X=Y=Z=0! \
+               \nThis assignment is probably \
+               \ngoing to make a mess with datamembers!\n\
+               \nPlease, check your tasks and the source code!");
+   SphericaltoCartesian();
+   CartesiantoCylindrical();
+   if (gDebug) Printf("Debug: \n X=%f\n Y=%f\n Z=%f",X,Y,Z);
+}
+
+//___________Coordinates_Tools__________
+void Punto::CartesiantoSpherical() { 
+   /* Update spherical coordinates, managing exceptions and 
+   singularities.*/
+
+   if ( (SRadius=TMath::Sqrt(X*X+Y*Y+Z*Z))!=0 ) {
+
+      // Theta
+      Theta=TMath::ACos(Z/SRadius);
+
+      // Phi
+      if (X>0.) {
+         if (Y>0.) Phi=TMath::ATan(Y/X);               // X>0. et Y>0.
+         else {
+            if (Y==0.) Phi=0.;                         // X>0. et Y=0.
+            else Phi=2*TMath::Pi()+TMath::ATan(Y/X);   // X>0. et Y<0.
+         }
+      }
+      else {
+         if (X==0.) {
+            if (Y>0.) Phi=TMath::Pi()/2;               // X=0. et Y>0.
+            else {
+               if (Y==0.) Phi=Theta=0.;                // X=0. et Y=0. 
+               else Phi=TMath::Pi()+TMath::Pi()/2;     // X=0. et Y<0.
+            }
+         }
+         else { 
+            if (Y>0) Phi=TMath::Pi()+TMath::ATan(Y/X); // X<0. et Y>0.
+            else {
+               if (Y==0.) Phi=TMath::Pi();             // X<0. et Y=0. 
+               else Phi=TMath::Pi()+TMath::ATan(Y/X);  // X<0. et Y<0.
+            }
+         }
+      }
+   }
+   else {
+      Phi=0.;
+      Theta=0.;
+   }
+}
+
+void Punto::CartesiantoCylindrical() { 
+   /* Update spherical coordinates, managing exceptions and 
+   singularities. Note that the third component [i.e "Z"] is not
+   update, because is the same datamember used as cartesian coord.*/
+   
+   CRadius=TMath::Sqrt(X*X+Y*Y);
+   /* Phi is the angular coordinate. Please note that is equal to 
+   the spherical one. */
+   if (X>0.) {
+      if (Y>0.) Phi=TMath::ATan(Y/X);               // X>0. et Y>0.
+      else {
+         if (Y==0.) Phi=0.;                         // X>0. et Y=0.
+         else Phi=2*TMath::Pi()+TMath::ATan(Y/X);   // X>0. et Y<0.
+      }
+   }
+   else {
+      if (X==0.) {
+         if (Y>0.) Phi=TMath::Pi()/2;               // X=0. et Y>0.
+         else {
+            if (Y==0.) Phi=Theta=0.;                // X=0. et Y=0. 
+            else Phi=TMath::Pi()+TMath::Pi()/2;     // X=0. et Y<0.
+         }
+      }
+      else { 
+         if (Y>0) Phi=TMath::Pi()+TMath::ATan(Y/X); // X<0. et Y>0.
+         else {
+            if (Y==0.) Phi=TMath::Pi();             // X<0. et Y=0. 
+            else Phi=TMath::Pi()+TMath::ATan(Y/X);  // X<0. et Y>0.
+         }
+      }
+   }
+}
+
+void Punto::SphericaltoCartesian() {
+   X=SRadius*TMath::Sin(Theta)*TMath::Cos(Phi);
+   Y=SRadius*TMath::Sin(Theta)*TMath::Sin(Phi);
+   Z=SRadius*TMath::Cos(Theta);
+}
+
+void Punto::CylindricaltoCartesian() {
+   X=CRadius*TMath::Cos(Phi);
+   Y=CRadius*TMath::Sin(Phi);
+}
