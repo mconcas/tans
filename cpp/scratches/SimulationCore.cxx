1c6abb54590bf26e48566ace272dc93af6fc7b8e
mconcas <mett1990@gmail.com>
2014-04-22 17:17:32 +0200

    SimCore initialization is live

--- cpp/scratches/SimulationCore.cxx
+++ cpp/scratches/SimulationCore.cxx
@@ -1,8 +1,37 @@
 #if !defined (__CINT__) || defined (__MAKECINT__)
 #include <TXMLEngine.h>
 #include <TObject.h>
-#include <SimulationCore.h>
+#include "SimulationCore.h"
 #endif
+
+XMLNodePointer_t FromChildToNextParent(TXMLEngine* engine, 
+   XMLNodePointer_t index) 
+{
+   index=engine->GetParent(index);
+   index=engine->GetNext(index);
+
+   return index;
+}
+
+XMLNodePointer_t DrainDetectorData(pipe_t &pipe, TXMLEngine* engine,
+   XMLNodePointer_t index)
+{  
+   pipe.fZetaLen=atof(engine->GetNodeContent(index));
+   index=engine->GetNext(index);
+   pipe.fPipeRad=atof(engine->GetNodeContent(index));
+   index=engine->GetNext(index);
+   TString materialname=engine->GetAttr(index,"type");
+   TString materialtype=engine->GetAttr(index,"title");
+   pipe.fMaterial=TMaterial(materialname.Data(),materialtype.Data(),
+      engine->GetIntAttr(index,"A"),
+      engine->GetIntAttr(index,"Z"),
+      atof(engine->GetAttr(index,"density")),
+      atof(engine->GetAttr(index,"radl")),
+      atof(engine->GetAttr(index,"inter")));
+   pipe.fThickness=atof(engine->GetNodeContent(index));
+
+   return index;
+}
 
 ClassImp(SimulationCore)
 
@@ -12,19 +41,83 @@ SimulationCore::SimulationCore() :
    fXMLEngine() { fParDataFileName=""; }
 
 SimulationCore::SimulationCore(TString Pardatafile) :
-    TObject(),
-    fParDataFileName(Pardatafile),
-    fXMLEngine() 
-{ 
-
-      // Read and store Simulation parameters.
-      fXMLEngine=new TXMLEngine();
-      fXmldocfileptr=fXMLEngine->ParseFile(fParDataFileName.Data());
-
-}
+   TObject(),
+   fParDataFileName(Pardatafile),
+   fXMLEngine() { }
 
 
 void SimulationCore::SetParamFile(TString Filename) 
 {
    fParDataFileName=Filename;
+}
+
+Int_t SimulationCore::Initialize()
+{
+   // Read and store Simulation parameters.
+   fXMLEngine=new TXMLEngine();
+   fXmlDocFileptr=fXMLEngine->ParseFile(fParDataFileName.Data());
+   
+   // Set index on root element. THe functions GetChild() and GetNext() allow to 
+   // browse the XMLdocument.
+   fIndexNodeptr=fXMLEngine->DocGetRootElement(fXmlDocFileptr);
+   fIndexNodeptr=fXMLEngine->GetChild(fIndexNodeptr);
+   
+   // Read and store «infile» name.
+   fInputData=fXMLEngine->GetAttrValue(fXMLEngine->GetFirstAttr(fIndexNodeptr));
+   fIndexNodeptr=fXMLEngine->GetChild(fIndexNodeptr);
+   if((TString)fXMLEngine->GetAttrValue(fXMLEngine->GetFirstAttr(fIndexNodeptr))
+      =="distribution") {
+      fMultDist=fXMLEngine->GetNodeContent(fIndexNodeptr);
+   } else {
+      Printf("Error: multiplicity histogram not specified.");
+      return -1;
+   }
+   fIndexNodeptr=fXMLEngine->GetNext(fIndexNodeptr);
+   if((TString)fXMLEngine->GetAttrValue(fXMLEngine->GetFirstAttr(fIndexNodeptr))
+      =="distribution") {
+      fEtaDist=fXMLEngine->GetNodeContent(fIndexNodeptr);
+   } else {
+      Printf("Error: η histogram not specified.");
+      return -1;
+   }
+
+   fIndexNodeptr=FromChildToNextParent(fXMLEngine,fIndexNodeptr);
+
+   // Get vertices parameters.
+   fDebug=fXMLEngine->GetIntAttr(fIndexNodeptr,"debug");
+   fNoiseLevel=fXMLEngine->GetIntAttr(fIndexNodeptr,"noise");
+   fNumVertices=fXMLEngine->GetIntAttr(fIndexNodeptr,"num");
+   fIndexNodeptr=fXMLEngine->GetChild(fIndexNodeptr);
+   fIndexNodeptr=fXMLEngine->GetNext(fIndexNodeptr);
+   if((TString)fXMLEngine->GetAttrValue(fXMLEngine->GetFirstAttr(fIndexNodeptr))
+      =="gaussian") fRmsX=atof(fXMLEngine->GetNodeContent(fIndexNodeptr));
+   else fRmsX=0.;
+   fIndexNodeptr=fXMLEngine->GetNext(fIndexNodeptr);
+   if((TString)fXMLEngine->GetAttrValue(fXMLEngine->GetFirstAttr(fIndexNodeptr))
+      =="gaussian") fRmsY=atof(fXMLEngine->GetNodeContent(fIndexNodeptr));
+   else fRmsY=0.;
+   fIndexNodeptr=fXMLEngine->GetNext(fIndexNodeptr);
+   if((TString)fXMLEngine->GetAttrValue(fXMLEngine->GetFirstAttr(fIndexNodeptr))
+      =="gaussian") fRmsZ=atof(fXMLEngine->GetNodeContent(fIndexNodeptr));
+   else fRmsZ=0.;
+   fIndexNodeptr=FromChildToNextParent(fXMLEngine,fIndexNodeptr);
+   fIndexNodeptr=fXMLEngine->GetChild(fIndexNodeptr);
+   fIndexNodeptr=DrainDetectorData(fBeampipe,fXMLEngine,fIndexNodeptr);
+   fIndexNodeptr=FromChildToNextParent(fXMLEngine,fIndexNodeptr);
+   fIndexNodeptr=fXMLEngine->GetChild(fIndexNodeptr);
+   fIndexNodeptr=DrainDetectorData(fFirstLayer,fXMLEngine,fIndexNodeptr);
+   fIndexNodeptr=FromChildToNextParent(fXMLEngine,fIndexNodeptr);
+   fIndexNodeptr=fXMLEngine->GetChild(fIndexNodeptr);
+   fIndexNodeptr=DrainDetectorData(fSecondLayer,fXMLEngine,fIndexNodeptr);
+   fIndexNodeptr=FromChildToNextParent(fXMLEngine,fIndexNodeptr);
+   if((TString)fXMLEngine->GetAttr(fIndexNodeptr,"dryrun")=="kFALSE")
+      fDryRun=kFALSE;
+   else fDryRun=kTRUE;
+   
+   if((TString)fXMLEngine->GetAttr(fIndexNodeptr,"montecarlo")=="kFALSE") 
+      fMontecarloTruth=kFALSE;
+      else fMontecarloTruth=kTRUE;
+   fOutFileName=(TString)fXMLEngine->GetNodeContent(fIndexNodeptr);
+   
+   return 0; 
 }
--- cpp/scratches/SimulationCore.h
+++ cpp/scratches/SimulationCore.h
@@ -3,8 +3,16 @@
 
 #include <TObject.h>
 #include <TXMLEngine.h>
+#include <TMaterial.h>
 #include <TSystem.h>
 
+struct pipe_t {
+   Int_t     fLayer;
+   TMaterial fMaterial;
+   Double_t  fZetaLen;
+   Double_t  fThickness;
+   Double_t  fPipeRad;
+};
 
 
 class SimulationCore : public TObject {
@@ -18,32 +26,39 @@ class SimulationCore : public TObject {
 
       //____________Setters______________________
       void SetParamFile(TString Filename);
+      Int_t Initialize();
 
       //____________Inline_Getters_______________
       TString             GetParamFile() { return fParDataFileName; }
-      XMLDocPointer_t     GetXMLDocPointer() { return fXmldocfileptr; }
+      XMLDocPointer_t     GetXMLDocPointer() { return fXmlDocFileptr; }
 
    private:
+
+      // XML Specifications 
       TString             fParDataFileName;    // XML filename.
       TXMLEngine          *fXMLEngine;         // XMLEngine ptr for xml.
-      XMLDocPointer_t     fXmldocfileptr;      // Pointer to the XML content.
+      XMLDocPointer_t     fXmlDocFileptr;      // Pointer to the XML content.
+      XMLNodePointer_t    fIndexNodeptr;       // Index running on XML element.
 
       // Parameters
-      Int_t    fDebug;
-      Int_t    fNuVertices;
-      Bool_t   fDryRun;
-      Bool_t   fMontecarloTruth;
-      Int_t    fNoiseLevel;
-      // Double_t fZetaLen;
-      // Double_t fBPipeRad;
-      // Double_t fFirstRad;
-      // Double_t fSecondRad;
-      TString  fOutFileName;
-      Int_t    fCustomMult;
-      Bool_t   fMultipleScat;
-      TString  fInputData;
-      TString  fMultDist;
-      TString  fEtaDist;
+      Int_t       fDebug;                      // Debug flag.
+      TString     fInputData;                  // Input file.
+      TString     fMultDist;                   // Multiplicity histogram name.
+      TString     fEtaDist;                    // η histogram name.
+      Int_t       fNumVertices;                // Number of events to generate.
+      Int_t       fNoiseLevel;                 // Noise level.           
+      Bool_t      fMultipleScat;               // Flag for multiple scattering.
+      pipe_t      fBeampipe;                   // Beampipe struct.
+      pipe_t      fFirstLayer;                 // First detecotr struct.
+      pipe_t      fSecondLayer;                // Second detector struct.
+      Bool_t      fDryRun;                     // Dry run flag.
+      Bool_t      fMontecarloTruth;            // Save Montecarlo truth flag.
+      TString     fOutFileName;                // Output file name.
+      Double_t    fRmsX;                       // Rms of X coordinate.       
+      Double_t    fRmsY;                       // Rms of Y coordinate. 
+      Double_t    fRmsZ;                       // Rms of Z coordinate.       
+      
+      
 
 
    ClassDef(SimulationCore,1)
